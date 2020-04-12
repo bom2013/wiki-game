3861753bfbe1d7b754669b4fc68fb900f30d2696
bom2013 <noam21288@gmail.com>
2020-04-12 21:59:04 +0300

    add option to stop game
    add option to stop game and go from popup to game_popup and reverse

---
+++ wiki game/js/game_popup.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('#stop-button').click(function() {
+    if (confirm('אתה בטוח שברצונך להפסיק את המשחק בעיצומו?')) {
+	chrome.storage.local.set({game_on: false});
+	chrome.browserAction.setPopup({popup: "../popup.html"});
+	window.close();
+    }
+  })
+})
--- wiki game/js/popup.js
+++ wiki game/js/popup.js
@@ -4,7 +4,9 @@ var port = chrome.runtime.connect({
 $(document).ready(function() {
   $('#new-game-button').click(function() {
 	chrome.tabs.create({ url: "https://he.wikipedia.org/wiki/%D7%9E%D7%99%D7%95%D7%97%D7%93:%D7%90%D7%A7%D7%A8%D7%90%D7%99" });
-    console.log("start game");
+    chrome.storage.local.set({game_on: true});
+	chrome.browserAction.setPopup({popup: "src/game_popup.html"});
+	console.log("start game");
     port.postMessage("START"); 
   });
 });
