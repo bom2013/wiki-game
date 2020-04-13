7c0573f426674b163262fc9a46676ca91466f9cc
bom2013 <noam21288@gmail.com>
2020-04-13 23:13:35 +0300

    add move increament

--- wiki game/content.js
+++ wiki game/content.js
@@ -1,30 +1,7 @@
-/*var clicks = 0;
-$('#mw-head').append("<div style='background-color: #3fb543;'><h1 style='padding: 10px; padding-left:15px; color:white; margin-left:15px; font-family: 'Montserrat', 'Helvetica Neue', Helvetica, Arial, sans-serif; color:#fec503; font-size: 20px;'>JA! YOU WIN!<button style='margin-left:20px; color:black; font-size:15px;'><a id='playagain' href='https://en.wikipedia.org/wiki/Special:Random'>Play again?</a></button></h1></div>");
-*/
-/* var gameState = false
-var port = chrome.runtime.connect({name: "background"});
-
-if(gameState)
-{
-	$('#mw-head').append("<div style='background-color: #3fb543;'><h1 style='padding: 10px; padding-left:15px; color:white; margin-left:15px; font-family: 'Montserrat', 'Helvetica Neue', Helvetica, Arial, sans-serif; color:#fec503; font-size: 20px;'>JA! YOU WIN!<button style='margin-left:20px; color:black; font-size:15px;'><a id='playagain' href='https://en.wikipedia.org/wiki/Special:Random'>Play again?</a></button></h1></div>");
-}
-
-port.onMessage.addListener(function(state)
-{
-	if(state == "START")
-	{
-		console.log("Reset content to start game");
-		gameState = true
-	}
-});
- */
-
-//open port
-var port = chrome.runtime.connect({name: "background"});
-
 var srcPage;
 var desPage;
 var moves;
+
 
 //inject the data table and updating local variables if not done yet (source and destination)
 chrome.storage.local.get("game_on", function(g_s) {
@@ -54,9 +31,10 @@ chrome.storage.local.get("game_on", function(g_s) {
 					chrome.storage.local.get("moves", function(m)
 					{
 						moves = m.moves;
+						chrome.storage.local.set({moves: (parseInt(moves)+1).toString()});
 						$('<div id="content-injected-wiki-game"> <style>#content-injected-wiki-game{direction: rtl; text-align: center; background-color: #007C52; font-size: 20px;}#content-injected-wiki-game table, #content-injected-wiki-game th, #content-injected-wiki-game td{border: 1px solid black; border-collapse: collapse;}</style> <table style="width:100%"> <tr> <th>דף מקור</th> <th>צעדים</th> <th>דף יעד</th> </tr><tr> <td id="src-data-td"></td><td id="move-data-td"></td><td id="des-data-td"></td></tr></table></div>').insertAfter("#firstHeading");
 						$("#src-data-td").append(srcPage);
-						$("#move-data-td").append(moves);
+						$("#move-data-td").append((parseInt(moves)+1).toString());
 						$("#des-data-td").append(desPage);
 					});
 				});
@@ -65,4 +43,3 @@ chrome.storage.local.get("game_on", function(g_s) {
 		});
 	}
  });
-
--- wiki game/js/game_popup.js
+++ wiki game/js/game_popup.js
@@ -1,8 +1,26 @@
-$(document).ready(function() {
-  $('#stop-button').click(function() {
-    if (confirm('אתה בטוח שברצונך להפסיק את המשחק בעיצומו?')) {
-	chrome.storage.local.set({game_on: false});
-	chrome.browserAction.setPopup({popup: "../popup.html"});
-    }
-  })
-})
+var port = chrome.runtime.connect({name: "background"});
+
+$(document).ready(function()
+ {
+	chrome.storage.local.get("des_page", function(data)
+	{
+		$("#des-p").append(data.des_page);
+	});
+	chrome.storage.local.get("src_page", function(data)
+	{
+		$("#src-p").append(data.src_page);
+	});
+	chrome.storage.local.get("moves", function(data)
+	{
+		$("#move-p").append(data.moves);
+	});
+	$('#stop-button').click(function() 
+	{
+		if (confirm('אתה בטוח שברצונך להפסיק את המשחק בעיצומו?')) 
+		{
+			chrome.storage.local.set({game_on: false});
+			port.postMessage("STOP");
+			chrome.browserAction.setPopup({popup: "../popup.html"});
+		}
+	});
+});
