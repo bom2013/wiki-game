01401a58fb3258a7b17be2f09626f8aa7d55ded9
bom2013 <noam21288@gmail.com>
2020-04-16 22:05:17 +0300

    add wiki api functions

--- wiki game/background.js
+++ wiki game/background.js
@@ -1,3 +1,28 @@
+async function getRandomPage() {
+  /**
+   * Send the request to get the images
+   */
+  var res;
+  await fetch("https://he.wikipedia.org/w/api.php?action=query&list=random&format=json&rnnamespace=0&rnlimit=1" + "&origin=*")
+    .then(function(response) {
+      return response.json();
+    })
+    .then((response) => {
+      var pages = response.query.random; // Process the output to get the image names
+      res = pages[0].title;
+    });
+  console.log(res);
+  return res;
+}
+
+function getPageNameFromWikiLink(wikiLink) {
+  return decodeURIComponent(wikiLink).replace("https://he.wikipedia.org/wiki/", "").split("_").join(" ");
+}
+
+function getWikiLinkFromPageName(name) {
+  return "https://he.wikipedia.org/wiki/" + name.replace(" ", "_");
+}
+
 var pageList = [];
 
 chrome.runtime.onConnect.addListener(function(port) {
