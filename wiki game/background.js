5dfaabc10e65fc5d901755952af3394de88e9222
bom2013 <noam21288@gmail.com>
2020-04-17 10:57:36 +0300

    convert getRandomPage to sync function with callback, add comments and fix wiki functions

--- wiki game/background.js
+++ wiki game/background.js
@@ -1,26 +1,38 @@
-async function getRandomPage() {
-  /**
-   * Send the request to get the images
-   */
+/**
+ * Get random wiki page
+ *
+ * @param {function} callback The function callback in the end of the function
+ */
+function getRandomPage(callback) {
   var res;
-  await fetch("https://he.wikipedia.org/w/api.php?action=query&list=random&format=json&rnnamespace=0&rnlimit=1" + "&origin=*")
+  fetch("https://he.wikipedia.org/w/api.php?action=query&list=random&format=json&rnnamespace=0&rnlimit=1" + "&origin=*")
     .then(function(response) {
       return response.json();
     })
     .then((response) => {
       var pages = response.query.random; // Process the output to get the image names
       res = pages[0].title;
+      console.log("Finish to get the data(" + res + "), call the call back function");
+      callback(getWikiLinkFromPageName(res));
     });
-  console.log(res);
-  return res;
 }
 
+/**
+ * Get the wiki page name from wiki link
+ * @param  {String} wikiLink The wiki link of the page
+ * @return {String} The wiki page name of wikiLink
+ */
 function getPageNameFromWikiLink(wikiLink) {
   return decodeURIComponent(wikiLink).replace("https://he.wikipedia.org/wiki/", "").split("_").join(" ");
 }
 
+/**
+ * Get the wiki link from wiki page name
+ * @param  {String} name The wiki page name
+ * @return {String}      The wiki link of name
+ */
 function getWikiLinkFromPageName(name) {
-  return "https://he.wikipedia.org/wiki/" + name.replace(" ", "_");
+  return "https://he.wikipedia.org/wiki/" + name.split(" ").join("_");
 }
 
 var pageList = [];
