1122020619698d16450d980351d6796287ec1059
bom2013 <noam21288@gmail.com>
2020-04-20 21:55:19 +0300

    fix problem in moves show and format

--- wiki game/js/game_popup.js
+++ wiki game/js/game_popup.js
@@ -1,26 +1,20 @@
-var port = chrome.runtime.connect({name: "background"});
+var port = chrome.runtime.connect({ name: "background" });
 
-$(document).ready(function()
- {
-	chrome.storage.local.get("des_page_name", function(data)
-	{
+$(document).ready(function () {
+	chrome.storage.local.get("des_page_name", function (data) {
 		$("#des-p").append(data.des_page_name);
 	});
-	chrome.storage.local.get("src_page_name", function(data)
-	{
+	chrome.storage.local.get("src_page_name", function (data) {
 		$("#src-p").append(data.src_page_name);
 	});
-	chrome.storage.local.get("moves", function(data)
-	{
-		$("#move-p").append(data.moves);
+	chrome.storage.local.get("moves", function (data) {
+		$("#move-p").append(data.moves - 1);
 	});
-	$('#stop-button').click(function()
-	{
-		if (confirm('אתה בטוח שברצונך להפסיק את המשחק בעיצומו?'))
-		{
-			chrome.storage.local.set({game_on: false});
+	$('#stop-button').click(function () {
+		if (confirm('אתה בטוח שברצונך להפסיק את המשחק בעיצומו?')) {
+			chrome.storage.local.set({ game_on: false });
 			port.postMessage("STOP");
-			chrome.browserAction.setPopup({popup: "../popup.html"});
+			chrome.browserAction.setPopup({ popup: "../popup.html" });
 		}
 	});
 });
