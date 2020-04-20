d13b7fdbb4e1f4200b32acfcfcefc583f47fbbfb
bom2013 <noam21288@gmail.com>
2020-04-20 21:13:31 +0300

    Move wiki api function to separate file
    Move wiki api function from background.js to wiki_api_function.js and and add an appropriate reference in manifest.json

---
+++ wiki game/js/wiki_api_function.js
@@ -0,0 +1,59 @@
+/**
+ * Get random wiki page
+ *
+ * @param {function} callback The function callback in the end of the function
+ */
+function getRandomWikiPage(callback) {
+    var res;
+    fetch("https://he.wikipedia.org/w/api.php?action=query&list=random&format=json&rnnamespace=0&rnlimit=1" + "&origin=*")
+      .then(function (response) {
+        return response.json();
+      })
+      .then((response) => {
+        var pages = response.query.random; // Process the output to get the image names
+        res = pages[0].title;
+        console.log("Finish to get the data(" + res + "), call the call back function");
+        callback(getWikiLinkFromPageName(res));
+      });
+  }
+  
+  /**
+   * Get all internal link from wiki page
+   * 
+   * @param {string} pageName The wiki page name
+   * @param {function} callback The callback function that call when the function finish to get the links
+   */
+  function getAllLinkFromWikiPage(pageName, callback) {
+    var res;
+    fetch("https://he.wikipedia.org/w/api.php?action=query&format=json&titles=" + pageName + "&prop=links&pllimit=max" + "&origin=*")
+      .then(function (response) {
+        return response.json();
+      })
+      .then((response) => {
+        var links = response.query.pages;
+        var pageId = Object.keys(links)[0];
+        linksArray = links[pageId].links;
+        tArray = [];
+        for (var i in linksArray) { tArray.push(linksArray[i].title); }
+        callback(tArray);
+      });
+  }
+  
+  /**
+   * Get the wiki page name from wiki link
+   * @param  {String} wikiLink The wiki link of the page
+   * @return {String} The wiki page name of wikiLink
+   */
+  function getPageNameFromWikiLink(wikiLink) {
+    return decodeURIComponent(wikiLink).replace("https://he.wikipedia.org/wiki/", "").split("_").join(" ");
+  }
+  
+  /**
+   * Get the wiki link from wiki page name
+   * @param  {String} name The wiki page name
+   * @return {String}      The wiki link of name
+   */
+  function getWikiLinkFromPageName(name) {
+    return "https://he.wikipedia.org/wiki/" + name.split(" ").join("_");
+  }
+  
