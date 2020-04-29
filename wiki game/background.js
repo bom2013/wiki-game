80c7a906bfb9b8447a33fce4654256a973e00aac
bom2013 <noam21288@gmail.com>
2020-04-29 19:28:06 +0300

    add join game option

--- wiki game/background.js
+++ wiki game/background.js
@@ -37,7 +37,40 @@ chrome.runtime.onConnect.addListener(function (port) {
           port.postMessage("START");
         })
       })
-    } else if (message == "WIN") {
+    } else if (message.substr(0, 5) == "JOIN|") {
+      console.log("Join game - background.js");
+      pageList = [];
+      //init local variable
+      chrome.storage.local.set({
+        game_on: true
+      });
+      chrome.storage.local.set({
+        moves: "0"
+      });
+      chrome.storage.local.set({
+        page_list: []
+      });
+      arg = message.substr(5);
+      srcPage = arg.substr(0, arg.indexOf("|"));
+      desPage = arg.substr(arg.indexOf("|") + 1);
+      chrome.storage.local.set({
+        des_page: getWikiLinkFromPageName(desPage)
+      });
+      chrome.storage.local.set({
+        des_page_name: desPage
+      });
+      console.log("Set des page - background.js");
+      chrome.storage.local.set({
+        src_page: getWikiLinkFromPageName(srcPage)
+      });
+      chrome.storage.local.set({
+        src_page_name: srcPage
+      }
+      );
+      console.log("Set src page - background.js");
+      port.postMessage("START");
+    }
+    else if (message == "WIN") {
       console.log("Win game - background.js");
       chrome.storage.local.set({
         game_on: false
--- wiki game/js/popup.js
+++ wiki game/js/popup.js
@@ -2,11 +2,11 @@ var port = chrome.runtime.connect({
   name: "background"
 });
 
-port.onMessage.addListener(function(message) {
+port.onMessage.addListener(function (message) {
   if (message == "START") {
     console.log("Get START message - popup.js");
     //open new tab
-    chrome.storage.local.get("src_page", function(s_p) {
+    chrome.storage.local.get("src_page", function (s_p) {
       chrome.tabs.create({
         url: s_p.src_page
       });
@@ -14,15 +14,27 @@ port.onMessage.addListener(function(message) {
   }
 });
 
-$(document).ready(function() {
-  $('#new-game-button').click(function() {
+$(document).ready(function () {
+  $('#new-game-button').click(function () {
     $("#new-game-button").prop("disabled", true);
     //set the popup to game_popup.html
     chrome.browserAction.setPopup({
       popup: "src/game_popup.html"
     });
     //message and log
-    console.log("start game");
+    console.log("Start game");
     port.postMessage("RESTART");
+  });
+  $('#join-game-button').click(function () {
+    var srcPage = prompt("דף מקור", "");
+    var desPage = prompt("דף יעד", "");
+    //set the popup to game_popup.html
+    chrome.browserAction.setPopup({
+      popup: "src/game_popup.html"
+    });
+    //message and log
+    console.log("Join game, src=" + srcPage + ", des=" + desPage);
+    //Using "|" To separate the arguments because the character "|" cannot be in wiki page name
+    port.postMessage("JOIN|" + srcPage + "|" + desPage);
   });
 });
